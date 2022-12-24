3b203eb66cce939419da49d25cf65c9cf42c0108
admin <admin@adminnoMac-mini.local>
2022-12-25 01:23:27 +0900

    firebaseに匿名ログイン出来るようにした

--- lib/containers/viewer_peering.dart
+++ lib/containers/viewer_peering.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart' show HookWidget;
 import 'package:qr_flutter/qr_flutter.dart';
+import '../utilities/hooks.dart';
+import '../redux/states/viewer.dart';
 
-class ViewerPeeringPage extends StatelessWidget {
+class ViewerPeeringPage extends HookWidget {
   ViewerPeeringPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const deviceId = "1234567890";
+    final deviceId =
+        useSelector<ViewerState, String>((state) => state.deviceId)!;
 
     return Scaffold(
       body: Center(
--- lib/viewer_main.dart
+++ lib/viewer_main.dart
@@ -1,8 +1,35 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import './redux/sample_store.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import './containers/viewer_root.dart';
+import 'redux/viewer_store.dart';
 
-void main() {
+// (参照) https://firebase.flutter.dev/docs/auth/start/#optional-prototype-and-test-with-firebase-local-emulator-suite
+bool shouldUseFirebaseEmulator = false;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (!kIsWeb) {
+    await Firebase.initializeApp();
+  } else {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: String.fromEnvironment("apiKey"),
+        appId: String.fromEnvironment("appId"),
+        messagingSenderId: String.fromEnvironment("messagingSenderId"),
+        projectId: String.fromEnvironment("projectId"),
+        authDomain: String.fromEnvironment("authDomain"),
+        databaseURL: String.fromEnvironment("databaseURL"),
+      ),
+    );
+  }
+
+  if (shouldUseFirebaseEmulator) {
+    await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+  }
+
   runApp(const MyApp());
 }
 
@@ -11,7 +38,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SampleStoreProvider(
+    return ViewerStoreProvider(
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
